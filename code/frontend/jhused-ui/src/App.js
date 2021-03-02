bf934757c922d6065c67901270eba621787fc5c3
Samantha Fu <zongming04@gmail.com>
2021-03-02 16:53:14 -0500

    remove extraneous code

--- code/frontend/jhused-ui/src/App.js
+++ code/frontend/jhused-ui/src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
-import Location from "./Components/Location";
-import Carousel from "./Components/Carousel";
 import "./App.css";
-
-import image1 from "./images/furniture/desk.jpg";
-import image2 from "./images/furniture/desk2.jpg";
-import image3 from "./images/furniture/desk3.jpg";
-import image4 from "./images/furniture/desk4.jpg";
 
 const axios = require("axios").default;
 axios.defaults.baseURL = "https://jhused-api-server.herokuapp.com/";
 // axios.defaults.baseURL = "http://localhost:4567/";
-
-
-const items = [image1, image2, image3, image4];
 class App extends React.Component {
   constructor() {
     super();
@@ -37,7 +27,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Carousel images={items}/>
+        
       </div>
     );
   }
