139dd0f8d011499b9652d3dae63e7aa0167eb010
Samantha Fu <zongming04@gmail.com>
2021-03-02 16:50:21 -0500

    refactored to use props

--- code/frontend/jhused-ui/src/App.js
+++ code/frontend/jhused-ui/src/App.js
@@ -4,10 +4,17 @@ import Location from "./Components/Location";
 import Carousel from "./Components/Carousel";
 import "./App.css";
 
+import image1 from "./images/furniture/desk.jpg";
+import image2 from "./images/furniture/desk2.jpg";
+import image3 from "./images/furniture/desk3.jpg";
+import image4 from "./images/furniture/desk4.jpg";
+
 const axios = require("axios").default;
 axios.defaults.baseURL = "https://jhused-api-server.herokuapp.com/";
 // axios.defaults.baseURL = "http://localhost:4567/";
 
+
+const items = [image1, image2, image3, image4];
 class App extends React.Component {
   constructor() {
     super();
@@ -30,9 +37,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="carousel-container">
-        <Carousel />
-        </div>
+        <Carousel images={items}/>
       </div>
     );
   }
--- code/frontend/jhused-ui/src/Components/Carousel.js
+++ code/frontend/jhused-ui/src/Components/Carousel.js
@@ -5,42 +5,34 @@ import {
   Slide,
   ButtonBack,
   ButtonNext,
-  Image
+  Image,
 } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
-import "./Carousel.css"
+import "./Carousel.css";
+import PrevButton from "../images/carousel-button-prev.png";
+import NextButton from "../images/carousel-button-next.png";
 
-import image1 from "../images/furniture/desk.jpg";
-import image2 from "../images/furniture/desk2.jpg";
-import image3 from "../images/furniture/desk3.jpg";
-import image4 from "../images/furniture/desk4.jpg";
-
-const items = [
-  <img src={image1} className="sliderimg" />,
-  <img src={image2} className="sliderimg" />,
-  <img src={image3} className="sliderimg" />,
-  <img src={image4} className="sliderimg" />,
-];
-
-class Carousel extends React.Component {
-  render() {
-    return (
+const Carousel = (props) => {
+  return (
+    <div className="carousel-container">
       <CarouselProvider
         naturalSlideWidth={100}
         naturalSlideHeight={100}
-        totalSlides={3}
-      >    
+        totalSlides={props.images.length}
+      >
         <Slider>
-          <Slide index={0} className="slide">  <Image src={image1} hasMasterSpinner={true} className="sliderimg"/> </Slide>
-          <Slide index={1} className="slide"><Image src={image2} hasMasterSpinner={true} className="sliderimg"/></Slide>
-          <Slide index={2} className="slide"><Image src={image3} hasMasterSpinner={true} className="sliderimg"/></Slide>
+          {props.images.map((item, index) => (
+            <Slide index={index} className="slide">
+              <Image src={item} hasMasterSpinner={true} className="sliderimg" />
+            </Slide>
+          ))}
         </Slider>
-        
-        <ButtonBack>Back</ButtonBack>
-        <ButtonNext>Next</ButtonNext>
+
+        <ButtonBack className="carousel-button"> <Image src={PrevButton} hasMasterSpinner={true} /></ButtonBack>
+        <ButtonNext className="carousel-button"><Image src={NextButton} hasMasterSpinner={true} /></ButtonNext>
       </CarouselProvider>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Carousel;
