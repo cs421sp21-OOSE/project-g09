461d24b219b781ed67ebdda844e8f94f203da8a4
Samantha Fu <zongming04@gmail.com>
2021-03-05 01:08:57 -0500

    adjusted styling

--- code/frontend/jhused-ui/src/components/PostDetails.js
+++ code/frontend/jhused-ui/src/components/PostDetails.js
@@ -11,12 +11,12 @@ const PostDetails = (props) => {
       <div className="post-body">
         <a href="/"><img src={ExitPng} alt="x" className="exit"></img> </a>
         <h1 className="post-title">{props.post.title}</h1>
-        <h1 className="post-price">{props.post.price}</h1>
+        <h1 className="post-price">${props.post.price}</h1>
         <div className="post-content-left">
           <Carousel images={props.post.images} />
         </div>
         <div className="post-content-right">
-          <Location location={props.post.location} size="l" />
+          <Location location={props.post.location} size="s" />
           <div className="post-description">
             <p>{props.post.description} </p>
           </div>
