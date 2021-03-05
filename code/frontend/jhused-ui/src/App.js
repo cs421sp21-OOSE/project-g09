976d94178e2f1102b186744ba64a5374cce9f3c0
Samantha Fu <zongming04@gmail.com>
2021-03-05 01:08:49 -0500

    added dummy post info

--- code/frontend/jhused-ui/src/App.js
+++ code/frontend/jhused-ui/src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 import ImageGrid from "./ImageGrid";
 import { Switch, Route } from "react-router-dom";
 import Editor from "./components/Editor";
+import img1 from "./images/furniture/desk.jpg";
+import img2 from "./images/furniture/desk2.jpg";
+import img3 from "./images/furniture/desk3.jpg";
+import img4 from "./images/furniture/desk4.jpg";
+import PostDetails from "./components/PostDetails";
 
+const testPost = {
+  title: "Desk",
+  location: "Marylander",
+  price: 30,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  images: [img1, img2, img3, img4]
+};
 
 const axios = require("axios").default;
 //axios.defaults.baseURL = "https://jhused-api-server.herokuapp.com/";
@@ -30,6 +42,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ImageGrid} />
           <Route exact path="/editor" component={Editor} />
+          <Route exact path="/post/:postID" children={<PostDetails post={testPost}/>}></Route>
         </Switch>
       </div>
     );
