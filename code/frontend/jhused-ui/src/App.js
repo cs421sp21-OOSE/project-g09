524e8a2127df83cc28d8812c58a054f1d70712cd
Samantha Fu <zongming04@gmail.com>
2021-03-05 13:39:12 -0500

    changed app to function component for hook use

--- code/frontend/jhused-ui/src/App.js
+++ code/frontend/jhused-ui/src/App.js
@@ -1,40 +1,23 @@
 import React from "react";
 import "./App.css";
 import ImageGrid from "./components/ImageGrid";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Editor from "./components/Editor";
 import axios from "./util/axios";
 import PostDetails from "./components/PostDetails";
 
-class App extends React.Component {
-  constructor() {
-    super();
-  }
-
-  async componentDidMount() {
-    axios.get("").then((response) => {
-      console.log(response);
-      const message = response.data;
-      this.setState({
-        welcom: message.message,
-      });
-      console.log(this.state.welcom);
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <ImageGrid />
-          </Route>
-          <Route path="/post/:postID" component={PostDetails} />
-        </Switch>
-        <Route exact path="/editor" component={Editor} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <ImageGrid />
+        </Route>
+        <Route path="/post/:postID" component={PostDetails} />
+      </Switch>
+      <Route exact path="/editor" component={Editor} />
+    </div>
+  );
+};
 
 export default App;
