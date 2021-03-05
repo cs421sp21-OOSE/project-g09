5eff5eb09d7a804737d8b840900795aaa5a8db68
Samantha Fu <zongming04@gmail.com>
2021-03-05 13:15:35 -0500

    added state to post details, connected to db

--- code/frontend/jhused-ui/src/App.js
+++ code/frontend/jhused-ui/src/App.js
@@ -3,20 +3,8 @@ import "./App.css";
 import ImageGrid from "./components/ImageGrid";
 import { Switch, Route } from "react-router-dom";
 import Editor from "./components/Editor";
-import img1 from "./images/furniture/desk.jpg";
-import img2 from "./images/furniture/desk2.jpg";
-import img3 from "./images/furniture/desk3.jpg";
-import img4 from "./images/furniture/desk4.jpg";
+import axios from "./util/axios";
 import PostDetails from "./components/PostDetails";
-
-const testPost = {
-  title: "Desk",
-  location: "Marylander",
-  price: 30,
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  images: [img1, img2, img3, img4]
-};
-
 
 class App extends React.Component {
   constructor() {
@@ -38,14 +26,15 @@ class App extends React.Component {
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/" component={ImageGrid} />
-          <Route exact path="/editor" component={Editor} />
-          <Route exact path="/post/:postID" children={<PostDetails post={testPost}/>}></Route>
+          <Route exact path="/">
+            <ImageGrid />
+          </Route>
+          <Route path="/post/:postID" component={PostDetails} />
         </Switch>
+        <Route exact path="/editor" component={Editor} />
       </div>
     );
   }
 }
 
 export default App;
-
