9b7be3e9b77fb04ee686a54b4ed5d514d6ea9556
Junjie Yang <junjie.yang@jhu.edu>
2021-03-05 10:50:57 -0500

    button setup

--- code/frontend/jhused-ui/src/components/ImageGrid.js
+++ code/frontend/jhused-ui/src/components/ImageGrid.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {axios} from "../App"
+import {Button} from "react-bootstrap";
+
 
 class ImageGrid extends React.Component {
     state = {
@@ -14,14 +16,17 @@ class ImageGrid extends React.Component {
 
     render() {
         return (
-            <div className="img-grid">
-                { this.state.posts && this.state.posts.map(post => (
-                    <div className="img-wrap" key={post.uuid}>
-                        <a href= {'/api/posts/' + post.uuid}>
-                            <img src={post.imageUrls[0]} alt="pic" />
-                        </a>
-                    </div>
-                ))}
+            <div>
+                <Button className="post-button" onClick={event => window.location.href='/editor'}>Post</Button>
+                <div className="img-grid">
+                    { this.state.posts && this.state.posts.map(post => (
+                        <div className="img-wrap" key={post.uuid}>
+                            <a href= {'/api/posts/' + post.uuid}>
+                                <img src={post.imageUrls[0]} alt="pic" />
+                            </a>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
