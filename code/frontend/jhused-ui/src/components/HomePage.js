226b57ce131297502e1af3b29abab1e9a1e4a0a0
Junjie Yang <junjie.yang@jhu.edu>
2021-03-16 09:21:17 -0400

    search bar added: title search only

--- code/frontend/jhused-ui/src/components/HomePage.js
+++ code/frontend/jhused-ui/src/components/HomePage.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import ImageGrid from "./ImageGrid";
 import EditorPopup from "./EditorPopUp";
-//import { Button } from "react-bootstrap";
 import "./HomePage.css";
+import axios from "../util/axios";
 
 const HomePage = () => {
 
   const [editorLive, setEditorLive] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchedPosts, setSearchPosts] = useState([]);
+
+  useEffect(() => {
+      axios.get("/api/posts").then((response) => {
+          setPosts(response.data);
+      });
+  }, [])
+
+    useEffect( () => {
+        setSearchPosts( posts.filter( (post) => {
+            if (searchTerm === "") {
+                return post;
+            } else if (post.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return post;
+            }
+            else return null;
+        }) );
+    }, [posts, searchTerm])
+
 
   const handlePostBtnChange = () => {
     setEditorLive(!editorLive);
@@ -17,15 +38,22 @@ const HomePage = () => {
       <div className="home-page-header">
         <button
           className="post-button"
-          // onClick={(event) => (window.location.href = "/editor")}
           onClick={handlePostBtnChange}
         >
           Post
         </button>
+          <div className="searchbar">
+              <input
+                  type="text"
+                  placeholder="Search..."
+                  onChange={(event) => {
+                      setSearchTerm(event.target.value);
+                  }}
+              />
+          </div>
       </div>
       {editorLive ? <EditorPopup toggle={handlePostBtnChange}/> : null}
-      
-      <ImageGrid />
+      <ImageGrid posts={searchedPosts}/>
     </div>
   );
 };
