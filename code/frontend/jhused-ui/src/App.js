a49c10d153f3420e7594210e8c185de8121f96d6
Samantha Fu <zongming04@gmail.com>
2021-03-24 20:27:47 -0400

    set up user context

--- code/frontend/jhused-ui/src/App.js
+++ code/frontend/jhused-ui/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./App.css";
 import HomePage from "./components/HomePage";
 import { Switch, Route, useLocation } from "react-router-dom";
 import PostDetails from "./components/PostDetails";
 import UserProfile from "./components/UserProfile";
-import logo from "./images/logo.png";
-import Editor from "./components/Editor";
+import Header from "./components/Header";
+
+
 
 const App = () => {
   const location = useLocation();
@@ -14,14 +15,15 @@ const App = () => {
   return (
     <div className="App">
       <div className="jhused-header">
-      <a href="/"><img className="jhused-logo" src={logo} alt="logo"/> </a>
       </div>
       <Switch location={background || location}>
         <Route exact path="/">
           <HomePage />
         </Route>
-        <Route exact path="/user/:userID" component={UserProfile}/>
-
+        <Route exact path="/user/:userID" component={UserProfile} />
+        <Route exact path="/test">
+          <Header />
+        </Route>
       </Switch>
 
       {background && <Route path="/post/:postID" children={<PostDetails />} />}
