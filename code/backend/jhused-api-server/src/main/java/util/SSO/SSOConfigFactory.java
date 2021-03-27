eb6a0d08a418ffb81b418733f2e929418fc04143
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-03-26 17:02:30 -0700

    added final parts in Config build witin SSOConfigFactory.

--- code/backend/jhused-api-server/src/main/java/util/SSO/SSOConfigFactory.java
+++ code/backend/jhused-api-server/src/main/java/util/SSO/SSOConfigFactory.java
@@ -1,7 +1,14 @@
 package util.SSO;
 
+import org.pac4j.core.authorization.authorizer.RequireAnyRoleAuthorizer;
+import org.pac4j.core.client.Clients;
+import org.pac4j.core.client.direct.AnonymousClient;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.config.ConfigFactory;
+import org.pac4j.core.credentials.TokenCredentials;
+import org.pac4j.core.matching.matcher.PathMatcher;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.util.CommonHelper;
 import org.pac4j.saml.client.SAML2Client;
 import org.pac4j.saml.config.SAML2Configuration;
 import spark.TemplateEngine;
@@ -27,6 +34,24 @@ public class SSOConfigFactory implements ConfigFactory {
         cfg.setServiceProviderMetadataPath("sp-metadata.xml");
         final SAML2Client saml2Client = new SAML2Client(cfg);
 
-        return null;
+        final HeaderClient headerClient = new HeaderClient("Authorization", (credentials, ctx) -> {
+            final String token = ((TokenCredentials) credentials).getToken();
+            if (CommonHelper.isNotBlank(token)) {
+                final CommonProfile profile = new CommonProfile();
+                profile.setId(token);
+                credentials.setUserProfile(profile);
+            }
+        });
+
+        final Clients clients = new Clients("http://localhost:8080/callback", oidcClient, saml2Client, facebookClient,
+                twitterClient, formClient, indirectBasicAuthClient, casClient, parameterClient, directBasicAuthClient, new AnonymousClient(),
+                headerClient);
+
+        final Config config = new Config(clients);
+        config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("ROLE_ADMIN"));
+        config.addAuthorizer("custom", new CustomAuthorizer());
+        config.addMatcher("excludedPath", new PathMatcher().excludeRegex("^/facebook/notprotected$"));
+        config.setHttpActionAdapter(new DemoHttpActionAdapter(templateEngine));
+        return config;
     }
 }
