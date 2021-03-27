0ccb2860050eb0aa3fbc0a4e8f5e7e3dcdb6913f
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-03-26 17:32:13 -0700

    removed extra clients from Clients function in SSO interface. Removed extra edit in config initializer. Updated SAML2 config to match #119

--- code/backend/jhused-api-server/src/main/java/util/SSO/SSOConfigFactory.java
+++ code/backend/jhused-api-server/src/main/java/util/SSO/SSOConfigFactory.java
@@ -9,6 +9,7 @@ import org.pac4j.core.credentials.TokenCredentials;
 import org.pac4j.core.matching.matcher.PathMatcher;
 import org.pac4j.core.profile.CommonProfile;
 import org.pac4j.core.util.CommonHelper;
+import org.pac4j.http.client.direct.HeaderClient;
 import org.pac4j.saml.client.SAML2Client;
 import org.pac4j.saml.config.SAML2Configuration;
 import spark.TemplateEngine;
@@ -27,31 +28,27 @@ public class SSOConfigFactory implements ConfigFactory {
     @Override
     public Config build(Object... parameters) {
 
-        final SAML2Configuration cfg = new SAML2Configuration("resource:samlKeystore.jks", "pac4j-demo-passwd",
-                "pac4j-demo-passwd", "resource:metadata-okta.xml");
+        //used the config from #119 in github
+        final SAML2Configuration cfg =
+                new SAML2Configuration(
+                        "resource" + ":samlKeystore.jks",
+                        "k-d1bf-i4s7*sd5fj",
+                        "k-d1bf-i4s7*sd5fj",
+                        "https://idp.jh.edu/idp/shibboleth");
+
         cfg.setMaximumAuthenticationLifetime(3600);
-        cfg.setServiceProviderEntityId("http://localhost:8080/callback?client_name=SAML2Client");
+        cfg.setServiceProviderEntityId("https://jhused-api-server.herokuapp.com/callback?client_name=SAML2Client");
         cfg.setServiceProviderMetadataPath("sp-metadata.xml");
-        final SAML2Client saml2Client = new SAML2Client(cfg);
+        final SAML2Client saml2Client  = new SAML2Client(cfg);
 
-        final HeaderClient headerClient = new HeaderClient("Authorization", (credentials, ctx) -> {
-            final String token = ((TokenCredentials) credentials).getToken();
-            if (CommonHelper.isNotBlank(token)) {
-                final CommonProfile profile = new CommonProfile();
-                profile.setId(token);
-                credentials.setUserProfile(profile);
-            }
-        });
+        //only Saml2 client.
+        final Clients clients = new Clients("http://localhost:8080/callback", saml2Client);
 
-        final Clients clients = new Clients("http://localhost:8080/callback", oidcClient, saml2Client, facebookClient,
-                twitterClient, formClient, indirectBasicAuthClient, casClient, parameterClient, directBasicAuthClient, new AnonymousClient(),
-                headerClient);
-
+        //TODO figure out this authorizer stuff
         final Config config = new Config(clients);
         config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("ROLE_ADMIN"));
         config.addAuthorizer("custom", new CustomAuthorizer());
-        config.addMatcher("excludedPath", new PathMatcher().excludeRegex("^/facebook/notprotected$"));
-        config.setHttpActionAdapter(new DemoHttpActionAdapter(templateEngine));
+        config.setHttpActionAdapter(new HttpActionAdapter(templateEngine));
         return config;
     }
 }
