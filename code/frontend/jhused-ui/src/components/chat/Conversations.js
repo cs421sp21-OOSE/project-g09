9c918ee560b75a4970b820c7632268b796c43a65
Junjie Yang <junjie.yang@jhu.edu>
2021-03-29 16:23:40 -0400

    made conversations selectable

--- code/frontend/jhused-ui/src/components/chat/Conversations.js
+++ code/frontend/jhused-ui/src/components/chat/Conversations.js
@@ -2,12 +2,16 @@ import {ListGroup} from "react-bootstrap";
 import {useConversations} from "../../state/ConversationsProvider";
 
 const Conversations = () => {
-  const { conversations } = useConversations()
+  const { conversations, selectConversationIndex } = useConversations()
 
   return (
     <ListGroup variant="flush">
       {conversations.map((conversation, index) => (
-        <ListGroup.Item key={index}>
+        <ListGroup.Item
+          key={index}
+          action
+          onClick={() => selectConversationIndex(index)}
+          active={conversation.selected}>
           {conversation.recipients.map(recipient => recipient.name).join(', ')}
         </ListGroup.Item>
       ))}
