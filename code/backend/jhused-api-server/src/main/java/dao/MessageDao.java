2fe561b7f7d287b4a044f07b00c47cc76f9e13aa
Bohua <bwan2@jhu.edu>
2021-04-03 01:00:51 +0800

    implement message Dao, JdbiMessageDao

---
+++ code/backend/jhused-api-server/src/main/java/dao/MessageDao.java
@@ -0,0 +1,87 @@
+package dao;
+
+import exceptions.DaoException;
+import model.Message;
+
+import java.util.List;
+
+public interface MessageDao {
+  /**
+   * Add one message to database.
+   * @param message a list of message objects.
+   * @return  message object added.
+   * @throws DaoException
+   */
+  Message create(Message message) throws DaoException;
+
+  /**
+   * Add a list of messages.
+   * @param messages a list of message objects.
+   * @return a list of message objects added.
+   * @throws DaoException
+   */
+  List<Message> create(List<Message> messages) throws DaoException;
+
+  /**
+   * read a message given message's id
+   * @param id message's id
+   * @return the message of that id
+   * @throws DaoException
+   */
+  Message read(String id) throws DaoException;
+
+  /**
+   * read all messages
+   * @return all messages in database
+   * @throws DaoException
+   */
+  List<Message> readAll() throws DaoException;
+
+  /**
+   * return any messages that is sent or received by a user given user id.
+   * @param userId user's id
+   * @return a list of messages of that user
+   * @throws DaoException
+   */
+  List<Message> readAllGivenSenderOrReceiverId(String userId) throws DaoException;
+
+  /**
+   * return any messages that is sent by a user given user id.
+   * @param senderId  the user id of the sender
+   * @return a list of messages of that sender
+   * @throws DaoException
+   */
+  List<Message> readAllGivenSenderId(String senderId) throws DaoException;
+
+  /**
+   * return any messages that is received by a user given user id.
+   * @param receiverId the user id of the receiver
+   * @return a list of messages of the receiver
+   * @throws DaoException
+   */
+  List<Message> readAllGivenReceiverId(String receiverId) throws DaoException;
+
+  /**
+   * update a message
+   * @param message the message to be updated
+   * @return message updated
+   * @throws DaoException
+   */
+  Message update(Message message) throws DaoException;
+
+  /**
+   * delete a message
+   * @param  id id of the message to be deleted
+   * @return the deleted message
+   * @throws DaoException
+   */
+  Message delete(String id) throws DaoException;
+
+  /**
+   * delete a list of messages
+   * @param messages a list of messages to be deleted
+   * @return a list of deleted messages
+   * @throws DaoException
+   */
+  List<Message> delete(List<String> ids) throws DaoException;
+}
---
+++ code/backend/jhused-api-server/src/main/java/dao/jdbiDao/JdbiMessageDao.java
@@ -0,0 +1,169 @@
+package dao.jdbiDao;
+
+import dao.MessageDao;
+import exceptions.DaoException;
+import model.Image;
+import model.Message;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.PreparedBatch;
+import org.jdbi.v3.core.statement.StatementException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class JdbiMessageDao implements MessageDao {
+  private Jdbi jdbi;
+
+  JdbiMessageDao(Jdbi jdbi) {
+    this.jdbi = jdbi;
+  }
+
+  @Override
+  public Message create(Message message) throws DaoException {
+    String sql = "WITH inserted AS("
+        + "INSERT INTO message(id, sender_id, receiver_id, message, read, sent_time) "
+        + "VALUES(:id, :senderId, :receiverId, :message, :read, :sentTime) RETURNING *) "
+        + "SELECT * FROM inserted;";
+    try {
+      return jdbi.inTransaction(handle -> handle.createQuery(sql).bindBean(message).mapToBean(Message.class))
+          .findOne()
+          .orElse(null);
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to create the message: " + ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public List<Message> create(List<Message> messages) throws DaoException {
+    String sql = "INSERT INTO message(id, sender_id, receiver_id, message, read, sent_time) "
+        + "VALUES(:id, :senderId, :receiverId, :message, :read, :sentTime);";
+    try {
+      List<Message> res;
+      if (messages.isEmpty()) {
+        res = new ArrayList<>();
+      } else {
+        res = jdbi.inTransaction(handle -> {
+          PreparedBatch batch = handle.prepareBatch(sql);
+          for (Message message : messages) {
+            if (message != null && (message.getId() == null || message.getId().length() != 36)) {
+              message.setId(UUID.randomUUID().toString());
+            }
+            batch.bindBean(message).add();
+          }
+          return batch.executeAndReturnGeneratedKeys().mapToBean(Message.class).list();
+        });
+      }
+      return res;
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to create the messages: " + ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public Message read(String id) throws DaoException {
+    String sql = "SELECT * FROM message WHERE message.id=:id;";
+    try {
+      return jdbi.inTransaction(handle -> handle.createQuery(sql).bind("id", id).mapToBean(Message.class))
+          .findOne().orElse(null);
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to read the message: " + ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public List<Message> readAll() throws DaoException {
+    String sql = "SELECT * FROM message;";
+    try {
+      return jdbi.inTransaction(handle -> handle.createQuery(sql).mapToBean(Message.class))
+          .list();
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to read the messages: " + ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public List<Message> readAllGivenSenderOrReceiverId(String userId) throws DaoException {
+    String sql = "SELECT * FROM message WHERE message.sender_id = :userId OR message.receiver_id = :userId;";
+    try {
+      return jdbi.inTransaction(handle -> handle.createQuery(sql).bind("userId", userId)
+          .mapToBean(Message.class)).list();
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to read the messages given userId:" + userId + " : " + ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public List<Message> readAllGivenSenderId(String senderId) throws DaoException {
+    String sql = "SELECT * FROM message WHERE message.sender_id = :userId;";
+    try {
+      return jdbi.inTransaction(handle -> handle.createQuery(sql).bind("userId", senderId)
+          .mapToBean(Message.class)).list();
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to read the messages given senderId:" + senderId + " : " + ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public List<Message> readAllGivenReceiverId(String receiverId) throws DaoException {
+    String sql = "SELECT * FROM message WHERE message.receiver_id = :userId;";
+    try {
+      return jdbi.inTransaction(handle -> handle.createQuery(sql).bind("userId", receiverId)
+          .mapToBean(Message.class)).list();
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to read the messages given receiverId:" + receiverId + " : " + ex.getMessage(),
+          ex);
+    }
+  }
+
+  @Override
+  public Message update(Message message) throws DaoException {
+    String sql = "WITH updated AS ("
+        + "UPDATE message SET message.message = :message RETURNING *)"
+        + "SELECT * FROM updated;";
+    try {
+      return jdbi.inTransaction(handle -> handle.createQuery(sql).bind("message", message.getMessage())
+          .mapToBean(Message.class)).findOne().orElse(null);
+    } catch (IllegalStateException | NullPointerException | StatementException ex) {
+      throw new DaoException("Unable to update the message: " + ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public Message delete(String id) throws DaoException {
+    String sql = "WITH deleted AS ("
+        + "DELETE FROM message WHERE message.id=:id RETURNING *)"
+        + "SELECT * FROM deleted;";
+    try {
+      return jdbi.inTransaction(handle ->
+          handle.createQuery(sql).bind("id", id).mapToBean(Message.class).findOne()).orElse(null);
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to delete the message with id: " + id
+          + " error message: " + ex.getMessage(), ex);
+    }
+  }
+
+  @Override
+  public List<Message> delete(List<String> ids) throws DaoException {
+    String sql = "DELETE FROM message WHERE message.id=:id;";
+    try {
+      return jdbi.inTransaction(handle -> {
+        List<Message> res;
+        if (ids.isEmpty()) {
+          res = new ArrayList<>();
+        } else {
+          PreparedBatch batch = handle.prepareBatch(sql);
+          for (String id : ids) {
+            batch.bind("id", id).add();
+          }
+          res = batch.executeAndReturnGeneratedKeys().mapToBean(Message.class).list();
+        }
+        return res;
+      });
+
+    } catch (IllegalStateException | StatementException ex) {
+      throw new DaoException("Unable to delete the messages: "
+          + " error message: " + ex.getMessage(), ex);
+    }
+  }
+}
