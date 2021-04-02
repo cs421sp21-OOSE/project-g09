428df922ee83aabccd34da064d9d7fb74a9284e9
Samantha Fu <zongming04@gmail.com>
2021-04-01 23:02:13 -0400

    adjust sold markers

--- code/frontend/jhused-ui/src/components/PostPreview.js
+++ code/frontend/jhused-ui/src/components/PostPreview.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Location from "./Location";
 
-
 const PostPreview = (props) => {
   const location = useLocation();
 
@@ -15,12 +14,26 @@ const PostPreview = (props) => {
             state: { background: location },
           }}
         >
-          <div className="flex h-60">
+          <div className="relative flex h-60">
             <img
               className="group-hover:text-red-700 my-2 w-full object-cover"
               src={props.post.images[0].url}
               alt="item preview"
             />
+
+            {props.post.saleState === "SOLD" ? (
+              <div className="absolute font-bold text-6xl text-red-600 origin-top-right inset-center">
+                {props.post.saleState}
+              </div>
+            ) : (
+              ""
+            )}
+
+            {props.post.saleState === "DEALING" ? (
+              <div className="absolute font-bold text-6xl text-red-600 origin-top-right inset-center">HOLD</div>
+            ) : (
+              ""
+            )}
           </div>
 
           <p className="group-hover:text-red-600"> {props.post.title}</p>
@@ -31,14 +44,12 @@ const PostPreview = (props) => {
       <Location location={props.post.location} />
 
       {props.displayEdit ? (
-        <Link
-          to={`/editor/${props.post.id}`}
-        >
+        <Link to={`/editor/${props.post.id}`}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
-            className="w-4 absolute text-red-600 origin-top-right absolute right-1 top-1 sm:w-8"
+            className="w-7 absolute text-red-600 origin-top-right absolute right-1 top-2 sm:w-8"
           >
             <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
             <path
@@ -48,17 +59,6 @@ const PostPreview = (props) => {
             />
           </svg>
         </Link>
-      ) : (
-        ""
-      )}
-      {props.post.saleState === "SOLD" ? (
-        <div className="post-card-sold">{props.post.saleState}</div>
-      ) : (
-        ""
-      )}
-
-      {props.post.saleState === "DEALING" ? (
-        <div className="post-card-sold">HOLD</div>
       ) : (
         ""
       )}
