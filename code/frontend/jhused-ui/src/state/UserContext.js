8245bb83d06c495a938357c27245f7629e03aea3
Samantha Fu <zongming04@gmail.com>
2021-04-02 23:17:06 -0400

    dependencies

--- code/frontend/jhused-ui/src/state/UserContext.js
+++ code/frontend/jhused-ui/src/state/UserContext.js
@@ -3,16 +3,6 @@ import axios from "../util/axios";
 import Icon from "../images/avatars/retsuko-1175534-1280x0.jpeg";
 import {Redirect} from "react-router-dom";
 
-// not totally accurate - just a stand in for now
-const fakeUser = {
-  id: "005222222222222222222222222222222222",
-  name: "Shae",
-  email: "abc6@yahoo.com",
-  profileImage:
-    "https://static.wikia.nocookie.net/asoiaf/images/3/3d/Shae_HBO.jpg/revision/latest/scale-to-width-down/300?cb=20120205045225&path-prefix=zh",
-  location: "the beach",
-};
-
 const Context = createContext();
 
 const Provider = (props) => {
