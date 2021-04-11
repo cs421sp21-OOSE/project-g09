b8f0ab08c3605dcb410e63e5b78e1e2e73fe82cb
Junjie Yang <junjie.yang@jhu.edu>
2021-04-11 15:15:46 -0400

    add unread badge to message

--- code/frontend/jhused-ui/src/components/Header.js
+++ code/frontend/jhused-ui/src/components/Header.js
@@ -138,8 +138,14 @@ const Header = (props) => {
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                         role="menuitem"
                       >
-                        <div className="ml-16 -mt-2 h-2 w-2 rounded-full bg-red-500">
-                        </div>
+                        {conversations.filter(conversation => {
+                          return conversation.messages.filter(message => {
+                            return message.read === false
+                          }).length !== 0
+                        }).length === 0 ? "" :
+                          <div className="ml-16 -mt-2 h-2 w-2 rounded-full bg-red-500">
+                          </div>
+                        }
                         Messages
                       </a>
 
