b5398386563c1557c4d2bfc5c065fb9af60f22e8
Giraffea1 <58932648+Giraffea1@users.noreply.github.com>
2021-04-12 00:34:23 -0700

    iteration4 uml

--- code/frontend/jhused-ui/src/components/chat/OpenConversation.js
+++ code/frontend/jhused-ui/src/components/chat/OpenConversation.js
@@ -38,6 +38,7 @@ const OpenConversation = () => {
                   
                   <div className="mx-2">
                     <img src={userContext.user.profileImage} alt="" className="h-6 w-6 sm:h-12 sm:w-12 rounded-full overflow-hidden object-cover"/>
+
                   </div>
                   
                   <div className="flex flex-col items-end">
