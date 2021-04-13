2c4b072aa4880c51bbfd5bfe446944729f2a2675
chu ding <cding91@gmail.com>
2021-04-13 00:19:31 -0400

    Make nav bar responsive

--- code/frontend/jhused-ui/src/components/chat/OpenConversation.js
+++ code/frontend/jhused-ui/src/components/chat/OpenConversation.js
@@ -41,7 +41,7 @@ const OpenConversation = () => {
                   </div>
                   
                   <div className="flex flex-col items-end">
-                    <div className="group flex items-center">
+                    <div className={`group flex items-center ${message.fromMe ? "flex-row" : "flex-row-reverse"}`}>
                       <button className="invisible group-hover:visible focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-400 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
