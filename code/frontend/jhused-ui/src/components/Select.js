b1ce2fad40ea62fbc472dfa03f889edc3f579b51
Samantha Fu <zongming04@gmail.com>
2021-04-14 17:06:14 -0400

    new select

--- code/frontend/jhused-ui/src/components/Select.js
+++ code/frontend/jhused-ui/src/components/Select.js
@@ -1,61 +1,93 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/solid";
 
-const Select = props => {
-  const [value, setValue] = useState('ALL')
-  const [isOpen, setIsOpen] = useState(false)
-  return (
-    <div className=''>
-      {' '}
-      <button
-        className='flex text-sm md:text-2xl w-30 rounded-full focus:outline-none'
-        onClick={() => {
-          setIsOpen(!isOpen)
-        }}
-      >
-        {' '}
-        {value}
-        <span className='sr-only'>Open user menu</span>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-          className='w-4 sm:w-6 justify-right'
+const Select = (props) => {
+  const [selected, setSelected] = useState(props.options[0]);
+  if (selected) {
+    return (
+      <div className="z-50 w-full">
+        <Listbox
+          value={selected}
+          onChange={(value) => {
+            setSelected(value);
+            props.setOptionSelected(value.name);
+          }}
         >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth={2}
-            d='M19 9l-7 7-7-7'
-          />
-        </svg>{' '}
-      </button>
-      <div
-        className={`z-50 user-menu mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none ${
-          isOpen ? 'open' : 'closed'
-        }`}
-        id='category-menu'
-        role='menu'
-        aria-orientation='vertical'
-        aria-labelledby='category-menu'
-        open={isOpen ? 'open' : 'closed'}
-      >
-
-        {props.menuItems.filter((a) => a != value).map(item => (
-          <div
-            className='block px-4 py-2 text-sm md:text-2xl text-gray-700 hover:bg-gray-100'
-            role='menuitem'
-            onClick={() => {
-              setValue(item)
-              setIsOpen(false)
-            }}
-          >
-            {item}
-          </div>
-        ))}
+          {({ open }) => (
+            <>
+              <div className="z-50 relative w-full">
+                <Listbox.Button className="flex w-full text-left bg-white rounded-lg focus:outline-none text-xl md:text-2xl">
+                  <div className="w-full flex justify-between content-center">
+                    <span className="block">{selected.name}</span>
+                    <span className="flex content-center z-50">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="w-4 sm:w-5 justify-right content-center"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M19 9l-7 7-7-7"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </Listbox.Button>
+                <Transition
+                  show={open}
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options
+                    static
+                    className="absolute right-0 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                  >
+                    {props.options.map((option, optionIdx) => {
+                      if (option.name !== selected.name) {
+                        return (
+                          <Listbox.Option
+                            key={optionIdx}
+                            className={({ active }) =>
+                              `${
+                                active
+                                  ? "hover:bg-gray-100"
+                                  : "text-cool-gray-900"
+                              }
+                    cursor-default select-none relative py-1 px-2 text-2xl`
+                            }
+                            value={option}
+                          >
+                            {({ selected, active }) => (
+                              <>
+                                <span
+                                  className={`${
+                                    selected ? "font-medium" : "font-normal"
+                                  } block truncate`}
+                                >
+                                  {option.name}
+                                </span>
+                              </>
+                            )}
+                          </Listbox.Option>
+                        );
+                      }
+                    })}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </>
+          )}
+        </Listbox>
       </div>
-    </div>
-  )
-}
+    );
+  } else return "";
+};
 
-export default Select
+export default Select;
