153aa6b6a31aa3f9473fa19262a959e883452532
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-14 19:49:50 -0700

    added Main function in SendMail

--- code/backend/jhused-api-server/src/main/java/email/SendMail.java
+++ code/backend/jhused-api-server/src/main/java/email/SendMail.java
@@ -1,4 +1,30 @@
 package email;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+
+import java.io.IOException;
+
 public class SendMail {
+
+    public static void main(Mail mail) {
+
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+
 }
