223909f88503908614b9b2f2068f874f63185825
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-14 20:29:20 -0700

    added WishlistEmailsTest

---
+++ code/backend/jhused-api-server/src/test/java/email/WishlistEmailsTest.java
@@ -0,0 +1,20 @@
+package email;
+
+import dao.PostDao;
+import dao.jdbiDao.JdbiPostDao;
+import model.Post;
+import model.User;
+import org.jdbi.v3.core.Jdbi;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.database.DataStore;
+import util.database.Database;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class WishlistEmailsTest {
+
+}
