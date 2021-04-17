56d82d300ec3b3972d0454b3d555e8c7355b05c2
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-16 18:31:45 -0700

    duplicated WishlistEmailsTest and changed into WelcomeEmailsTest

---
+++ code/backend/jhused-api-server/src/test/java/email/WelcomeEmailsTest.java
@@ -0,0 +1,70 @@
+package email;
+
+import dao.UserDao;
+import dao.jdbiDao.JdbiUserDao;
+import email.Wishlist.WishlistEmails;
+import model.Post;
+import model.User;
+import model.WishlistPostSkeleton;
+import org.jdbi.v3.core.Jdbi;
+import org.junit.jupiter.api.*;
+import util.database.DataStore;
+import util.database.Database;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class WelcomeEmailsTest {
+    private static List<User> sampleUsers;
+    private static List<Post> samplePosts;
+    private static List<WishlistPostSkeleton> sampleWishlistSkeleton;
+    private UserDao userDao;
+    private static Jdbi jdbi;
+
+    @BeforeAll
+    static void setSamples() {
+        sampleUsers = DataStore.sampleUsers();
+        samplePosts = DataStore.samplePosts();
+        sampleWishlistSkeleton = DataStore.sampleWishlistPosts();
+    }
+
+    @BeforeAll
+    static void connectToDatabase() throws URISyntaxException {
+        Database.USE_TEST_DATABASE = true; // use test dataset
+        Database.main(null); // reset dataset and add samples
+        jdbi = Database.getJdbi();
+    }
+
+    @BeforeEach
+    void injectDependency() throws URISyntaxException {
+        Database.truncateTables(jdbi);
+        Database.insertSampleUsers(jdbi, sampleUsers);
+        Database.insertSamplePosts(jdbi, samplePosts);
+        Database.insertSampleWishlistPosts(jdbi, sampleWishlistSkeleton);
+        userDao = new JdbiUserDao(jdbi);
+    }
+
+    @AfterAll
+    static void setUseProductionDatabase() {
+        Database.USE_TEST_DATABASE = false; // use production dataset
+    }
+
+    //@Test
+    void doNothing() {
+
+    }
+
+    //@Test
+    @DisplayName("send an email.")
+    void updateSendMail() throws IOException {
+        WishlistEmails.basicWishlistUpdateEmail(jdbi, "000000000000000000000000000000000000");
+    }
+
+    @Test
+    @DisplayName("send an email.")
+    void updateSendStyledMail() throws IOException {
+        WishlistEmails.styledWishlistUpdateEmail(jdbi, "000000000000000000000000000000000000");
+    }
+
+}
