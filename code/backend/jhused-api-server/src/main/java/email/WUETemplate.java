c56a84d578d7d0ff31c08b55dad24635bb4785ab
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-16 17:08:57 -0700

    finalized first iteration of styledWUEEMail

--- code/backend/jhused-api-server/src/main/java/email/WUETemplate.java
+++ code/backend/jhused-api-server/src/main/java/email/WUETemplate.java
@@ -31,21 +31,25 @@ public class WUETemplate {
     public static Mail styledWUEEmail(String userEmail) {
         Email from = new Email("jhusedemail@gmail.com");
         String subject = "JHUsed Wishlist Update";
-        Email to = new Email(userEmail);
-        Content content = new Content("text/plain", "One of your wishlist-ed posts has been updated!");
+        Email to = new Email("ldibern1@jh.edu");
+        Content content = new Content("text/plain", "");
         Mail mail = new Mail(from, subject, to, content);
-        return mail;
+
+        mail.setTemplateId("d-ba70e2cbac7b4a279e30a5e5119fb5bb");
+
+        //return mail;
 
 
 
-        Email from = new Email("test@example.com");
+        //example code.
+        /*Email from = new Email("test@example.com");
         String subject = "I'm replacing the subject tag";
         Email to = new Email("test@example.com");
         Content content = new Content("text/html", "I'm replacing the <strong>body tag</strong>");
         Mail mail = new Mail(from, subject, to, content);
         mail.personalization.get(0).addSubstitution("-name-", "Example User");
         mail.personalization.get(0).addSubstitution("-city-", "Denver");
-        mail.setTemplateId("13b8f94f-bcae-4ec6-b752-70d6cb59f932");
+        mail.setTemplateId("13b8f94f-bcae-4ec6-b752-70d6cb59f932");*/
 
 
 
