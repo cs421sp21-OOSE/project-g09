5211dd944df81b0a14fe6bcc30dcc68fd53bffb8
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-16 17:39:08 -0700

    removed example code from styledWUEEmail

--- code/backend/jhused-api-server/src/main/java/email/WUETemplate.java
+++ code/backend/jhused-api-server/src/main/java/email/WUETemplate.java
@@ -35,24 +35,10 @@ public class WUETemplate {
         Content content = new Content("text/html", "One of your wishlist posts has been updated!");
         Mail mail = new Mail(from, subject, to, content);
 
+        //set the templateId from the sendgrid website.
         mail.setTemplateId("d-ba70e2cbac7b4a279e30a5e5119fb5bb");
 
         return mail;
-
-
-
-        //example code.
-        /*Email from = new Email("test@example.com");
-        String subject = "I'm replacing the subject tag";
-        Email to = new Email("test@example.com");
-        Content content = new Content("text/html", "I'm replacing the <strong>body tag</strong>");
-        Mail mail = new Mail(from, subject, to, content);
-        mail.personalization.get(0).addSubstitution("-name-", "Example User");
-        mail.personalization.get(0).addSubstitution("-city-", "Denver");
-        mail.setTemplateId("13b8f94f-bcae-4ec6-b752-70d6cb59f932");*/
-
-
-
     }
 
 
