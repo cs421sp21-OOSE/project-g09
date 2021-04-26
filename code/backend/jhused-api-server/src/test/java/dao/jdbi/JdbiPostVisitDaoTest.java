419be4e58030ad8e577c0be349298729036d5c0b
Bohua <bwan2@jhu.edu>
2021-04-27 00:51:58 +0800

    JdbiPostVisitDao done, test pass

--- code/backend/jhused-api-server/src/test/java/dao/jdbi/JdbiPostVisitDaoTest.java
+++ code/backend/jhused-api-server/src/test/java/dao/jdbi/JdbiPostVisitDaoTest.java
@@ -1,6 +1,7 @@
 package dao.jdbi;
 
-import dao.jdbiDao.JdbiWishlistPostSkeletonDao;
+import dao.PostVisitDao;
+import dao.jdbiDao.JdbiPostVisitDao;
 import model.PostVisit;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.AfterAll;
@@ -11,12 +12,18 @@ import util.database.DataStore;
 import util.database.Database;
 
 import java.net.URISyntaxException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class JdbiPostVisitDaoTest {
 
   private static final List<PostVisit> samplePostVisits = DataStore.samplePostVisits();
   private static Jdbi jdbi;
+  private static PostVisitDao postVisitDao;
 
   @BeforeAll
   static void connectToDatabase() throws URISyntaxException {
@@ -29,6 +36,7 @@ public class JdbiPostVisitDaoTest {
   void injectDependency() throws URISyntaxException {
     Database.truncateTable(jdbi, "post_visit");
     Database.insertSamplePostVisit(jdbi, samplePostVisits);
+    postVisitDao = new JdbiPostVisitDao(jdbi);
   }
 
   @AfterAll
@@ -38,5 +46,55 @@ public class JdbiPostVisitDaoTest {
 
   @Test
   void doNothing() {
+  }
+
+  @Test
+  void createWorks() {
+    Database.truncateTable(jdbi, "post_visit");
+    for (PostVisit postVisit : samplePostVisits) {
+      assertEquals(postVisit, postVisitDao.create(postVisit));
+    }
+  }
+
+  @Test
+  void createDuplicateReturnNull() {
+    for (PostVisit postVisit : samplePostVisits) {
+      assertNull(postVisitDao.create(postVisit));
+    }
+  }
+
+  @Test
+  void readWorks() {
+    for (PostVisit postVisit : samplePostVisits) {
+      assertEquals(postVisit, postVisitDao.read(postVisit.getPostId(), postVisit.getUserId()));
+    }
+  }
+
+  @Test
+  void readReturnNullNonExisting() {
+    assertNull(postVisitDao.read("lsijfesjflij","JHUsedAdmin"));
+    assertNull(postVisitDao.read("3".repeat(36),"lsiefjleisjfis"));
+  }
+
+  @Test
+  void visitCountWorks() {
+    Map<String, Integer> viewCnt = new LinkedHashMap<>();
+    for(PostVisit postVisit:samplePostVisits){
+      Integer cnt = viewCnt.get(postVisit.getPostId());
+      if(cnt==null){
+        viewCnt.put(postVisit.getPostId(),1);
+      }else{
+        cnt+=1;
+        viewCnt.put(postVisit.getPostId(),cnt);
+      }
+    }
+    viewCnt.forEach((k,v)->{
+      assertEquals(v,postVisitDao.visitCount(k));
+    });
+  }
+
+  @Test
+  void NonExistingViewCount0() {
+    assertEquals(0,postVisitDao.visitCount("lsiefisejf"));
   }
 }
