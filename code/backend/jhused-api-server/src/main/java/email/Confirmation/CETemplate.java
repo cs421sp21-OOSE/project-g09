f26131846af4affc66829ce45245e1e3c315b640
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-29 01:08:00 -0700

    corrected bug where styled email would be sent in plain text rather than html

--- code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
+++ code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
@@ -32,7 +32,7 @@ public class CETemplate {
         Email from = new Email("jhusedemail@gmail.com", "JHUsed");
         String subject = "Email changed";
         Email to = new Email(oldUserEmail);
-        Content content = new Content("text/plain", "Your email has been changed! If this was not you, please reply to this email and we will fix the problem. We strongly recommend changing your account password immediately to prevent further " +
+        Content content = new Content("text/html", "Your email has been changed! If this was not you, please reply to this email and we will fix the problem. We strongly recommend changing your account password immediately to prevent further " +
                 "unauthorized access.");
         Mail mail = new Mail(from, subject, to, content);
 
