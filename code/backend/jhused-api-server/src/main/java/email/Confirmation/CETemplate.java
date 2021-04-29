203e5570bdc4cd2930db087e677371eaf1542cc2
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-29 10:13:12 -0700

    I think I have correctly added unsubscribe bypass for the confirmation emails. pushing to test.

--- code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
+++ code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
@@ -1,9 +1,7 @@
 package email.Confirmation;
 
 import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.ASM;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.*;
 
 public class CETemplate {
 
@@ -44,9 +42,22 @@ public class CETemplate {
         //set the templateId from the sendgrid website.
         mail.setTemplateId("d-bbc8e61119514a60be047bb9e913e50e");
 
+        //assign the unsubscribe groupId
         ASM asm = new ASM();
         asm.setGroupId(16635);
         mail.setASM(asm);
+
+        //create the mailSettings object
+        MailSettings settings = new MailSettings();
+
+        //create and assign the basic setting object to true
+        Setting setting = new Setting();
+        setting.setEnable(true);
+
+        //set this email's bypass list management to true.
+        settings.setBypassListManagement(setting);
+        mail.setMailSettings(settings);
+
 
         return mail;
     }
