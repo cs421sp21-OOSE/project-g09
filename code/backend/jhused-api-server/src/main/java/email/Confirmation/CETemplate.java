d89d8f716a1f71220ef1a9ef16e53b9b177b2e1f
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-29 00:16:56 -0700

    basicCEEMail now contains informational email body with instructions to reverse unauthroized email changes.

--- code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
+++ code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
@@ -13,6 +13,13 @@ public class CETemplate {
      * @return
      */
     public static Mail basicCEEmail(String oldUserEmail) {
+        Email from = new Email("jhusedemail@gmail.com", "JHUsed");
+        String subject = "Email changed";
+        Email to = new Email(oldUserEmail);
+        Content content = new Content("text/plain", "Your email has been changed! If this was not you, please reply to this email and we will fix the problem. We strongly recommend changing your account password immediately to prevent further " +
+                "unauthorized access.");
+        Mail mail = new Mail(from, subject, to, content);
 
+        return mail;
     }
 }
