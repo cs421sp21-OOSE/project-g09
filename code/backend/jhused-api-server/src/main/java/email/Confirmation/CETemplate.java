8487819ff197305100a26871b7d5cce697589d13
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-29 00:14:27 -0700

    wrote method basicCEEMail

--- code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
+++ code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
@@ -1,4 +1,18 @@
 package email.Confirmation;
 
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.ASM;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
 public class CETemplate {
+
+    /**
+     * Returns the most basic text email.
+     * @param oldUserEmail
+     * @return
+     */
+    public static Mail basicCEEmail(String oldUserEmail) {
+
+    }
 }
