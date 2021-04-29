eee2f0e04250e841dda11b9bff7c4e9b30d8df77
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-29 00:47:01 -0700

    dupicated basicCEEMail into styledCEEmail

--- code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
+++ code/backend/jhused-api-server/src/main/java/email/Confirmation/CETemplate.java
@@ -9,8 +9,8 @@ public class CETemplate {
 
     /**
      * Returns the most basic text email.
-     * @param oldUserEmail
-     * @return
+     * @param oldUserEmail to be notified
+     * @return mail
      */
     public static Mail basicCEEmail(String oldUserEmail) {
         Email from = new Email("jhusedemail@gmail.com", "JHUsed");
@@ -19,6 +19,25 @@ public class CETemplate {
         Content content = new Content("text/plain", "Your email has been changed! If this was not you, please reply to this email and we will fix the problem. We strongly recommend changing your account password immediately to prevent further " +
                 "unauthorized access.");
         Mail mail = new Mail(from, subject, to, content);
+
+        return mail;
+    }
+
+    /**
+     * Returns a styled email.
+     * @param oldUserEmail to be notified
+     * @return mail
+     */
+    public static Mail styledCEEmail(String oldUserEmail) {
+        Email from = new Email("jhusedemail@gmail.com", "JHUsed");
+        String subject = "Email changed";
+        Email to = new Email(oldUserEmail);
+        Content content = new Content("text/plain", "Your email has been changed! If this was not you, please reply to this email and we will fix the problem. We strongly recommend changing your account password immediately to prevent further " +
+                "unauthorized access.");
+        Mail mail = new Mail(from, subject, to, content);
+
+        //set the templateId from the sendgrid website.
+        mail.setTemplateId("d-f79d67f0ead94ff08bdff7bfef53ebd6");
 
         return mail;
     }
