568fba1c5f9c547bacae5864cbc0b93ce2d3127e
TalkingTurtle <113813877+TalkingTurtle@users.noreply.github.com>
2021-04-29 00:12:05 -0700

    wrote method basicConfirmationEMail

--- code/backend/jhused-api-server/src/main/java/email/Confirmation/ConfirmationEmails.java
+++ code/backend/jhused-api-server/src/main/java/email/Confirmation/ConfirmationEmails.java
@@ -1,4 +1,20 @@
 package email.Confirmation;
 
+import com.sendgrid.helpers.mail.Mail;
+import email.SendMail;
+import email.Welcome.WETemplate;
+
+import java.io.IOException;
+
 public class ConfirmationEmails {
+
+    /**
+     * Sends basic text emails to users
+     * @param oldUserEmail to confirm change
+     */
+    public static void basicConfirmationEmail(String oldUserEmail) throws IOException {
+
+
+
+    }
 }
